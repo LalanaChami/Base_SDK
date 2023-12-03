d5c1d53b72afe39fb54f07656c13df9c10f4b5c8
Lalana <t.lalana@theglobalmarketaccess.com>
2023-12-03 23:10:46 +0530

    Made the portfolio entity identifiable

--- Sources/Base_SDK/Models/PortfolioEntity.swift
+++ Sources/Base_SDK/Models/PortfolioEntity.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-public struct PortfolioEntity: Codable {
+public struct PortfolioEntity: Identifiable, Codable {
     
     public let id , headingTitile, icon, holdingCurency: String?
     public let netChangePercentage: Double?
